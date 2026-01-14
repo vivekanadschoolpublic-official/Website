c90c14e40489f6a775f515ffaab85e307325a599
vishudverma <244vishudverma@gmail.com>
2026-01-14 18:54:31 +0000

    completed shortening and contextualising the content

--- app.py
+++ app.py
@@ -4,20 +4,28 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-# Making variables to use the modularity and asset saving.
-index_title = "Vivekanand Public School"
-enquiry_title = "Enquiry"
+# Personal details that are unanimous along all the pages
+@app.context_processor
+def inject_site_details():
+    details = {
+        "address": "J.P. Vihar Colony, Mangla, Bilaspur(C.G.)",
+        "email": "vivekanand.school.public@gmail.com",
+        "contact": "(+91) 83052-27722",
+        "index_title": "Vivekanand Public School",
+        "enquiry_title": "Enquiry",
+    }
+    return dict(details=details)
 
 
 # This is for making the index page
 @app.route("/")
 def test_page():
-    return render_template("index.html", title=index_title)
+    return render_template("index.html")
 
 
 @app.route("/enquiry")
 def enquiry():
-    return render_template("enquiry.html", title=enquiry_title)
+    return render_template("enquiry.html")
 
 
 # This block allows you to run the application directly
